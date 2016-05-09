9559878899e971167b25bc29d4a988227b6dfab4
Evelyn <1305047236@qq.com>
2016-05-09 21:41:10 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by 木子里 on 2016/5/8.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
